937d3f4ff4614090e691e210ce48b1e2a02376b2
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-03-16 04:16:30 +0200

    Delete nc-headbag directory

--- nc-headbag/interface/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("message", (event) => {
-    let data = event.data;
-    if (data["action"] == "open") {
-        $(".container").css({"display": "block"});
-    } else if (data["action"] == "remove") { 
-        $(".container").css({"display": "none"});
-    }
-})
